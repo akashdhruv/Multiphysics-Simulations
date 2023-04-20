6c7567d0c06be4078b615c72243ca64c8c8af42a
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-04-20 17:15:03 -0500

    add flow boiling velocity

---
+++ analysis/OutflowTest/FlowBoiling-Velocity.py
@@ -0,0 +1,32 @@
+import re
+import os
+import json
+
+outflowVelDict = { "time" : [], "liq" : [], "gas" : []}
+outdir = "/home/akash/Desktop/Akash/Workbench/Multiphase-Simulations/simulation/FlowBoiling/Example2D/inletVelScale-1.0/jobnode.archive/2023-04-13"
+outfiles = ["slurm-1937.out"]
+
+for outfile in outfiles:
+    with open(os.path.join(outdir,outfile),"r") as ofile:
+        for line in ofile:
+
+            if re.search("SimTime", line):
+                outflowVelDict["time"].append(float(line.split()[-1]))
+
+            if re.search("Outlet Gas Velocity HIGH", line):
+                outflowVelDict["gas"].append(float(line.split()[5]))
+
+            if re.search("Outlet Liq Velocity HIGH", line):
+                outflowVelDict["liq"].append(float(line.split()[5]))
+
+    print(len(outflowVelDict["time"]),len(outflowVelDict["gas"]))
+
+
+skip = 3000
+outflowVelDict["time"] = outflowVelDict["time"][::skip]
+outflowVelDict["gas"] = outflowVelDict["gas"][::skip]
+outflowVelDict["liq"] = outflowVelDict["liq"][::skip]
+print(len(outflowVelDict["time"]),len(outflowVelDict["gas"]))
+
+with open("FlowBoiling-Velocity.json", "w") as velFile:
+    velFile.write(json.dumps(outflowVelDict))
