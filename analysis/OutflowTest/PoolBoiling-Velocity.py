540a35d4076ccc7c3e6781c732dcc883e08041fb
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-04-20 15:59:49 -0500

    add pool boiling analysis data

---
+++ analysis/OutflowTest/PoolBoiling-Velocity.py
@@ -0,0 +1,25 @@
+import re
+import os
+import json
+
+outflowVelDict = { "time" : [], "liq" : [], "gas" : []}
+outdir = "/home/akash/Desktop/Akash/Workbench/Multiphase-Simulations/simulation/PoolBoiling/Gravity-FC72-2D/gravY-1.0/jobnode.archive/2023-04-13"
+outfiles = ["slurm-1931.out","slurm-1938.out"]
+
+for outfile in outfiles:
+    with open(os.path.join(outdir,outfile),"r") as ofile:
+        for line in ofile:
+
+            if re.search("SimTime", line):
+                outflowVelDict["time"].append(line.split()[-1])
+
+            if re.search("Outlet Gas Velocity HIGH", line):
+                outflowVelDict["gas"].append(line.split()[5])
+
+            if re.search("Outlet Liq Velocity HIGH", line):
+                outflowVelDict["liq"].append(line.split()[5])
+
+    #print(len(outflowVelDict["time"]),len(outflowVelDict["gas"]))
+
+with open("PoolBoiling-Velocity.json", "w") as velFile:
+    velFile.write(json.dumps(outflowVelDict))
