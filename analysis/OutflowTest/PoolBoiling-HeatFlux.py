a650add229720bf442a635b3356a1a3eb756fa88
akashdhruv <adhruv@login3.summit.olcf.ornl.gov>
2023-04-26 12:30:36 -0400

    updates from summit

--- analysis/OutflowTest/PoolBoiling-HeatFlux.py
+++ analysis/OutflowTest/PoolBoiling-HeatFlux.py
@@ -2,9 +2,9 @@ import os
 import sys
 import json
 import itertools
-from scipy import signal
+#from scipy import signal
 import numpy
-import matplotlib.pyplot as pyplot
+#import matplotlib.pyplot as pyplot
 import boxkit
 
 
@@ -39,16 +39,16 @@ def getWallHflux(dataset):
 
 if __name__ == "__main__":
 
-    PROJECT_HOME = "/home/akash/Desktop/Akash/Workbench/Multiphase-Simulations"
+    PROJECT_HOME = "/gpfs/alpine/ast136/proj-shared/adhruv/Multiphase-Simulations"
 
     datasetLoc = {
         r"Buffer = 4.0": PROJECT_HOME
         + os.sep
         #+ "simulation/PoolBoiling/Gravity-FC72-2D/gravY-1.0/jobnode.archive/2023-04-13",
-        + "simulation/PoolBoiling/Gravity-FC72-2D/gravY-0.1",
+        + "simulation/PoolBoiling/Gravity-FC72-2D/gravY-0.02",
     }
 
-    fileNumList = [*range(60, 250)]
+    fileNumList = [*range(60, 285)]
 
     datasetDict = {}
     for datasetKey in datasetLoc.keys():
@@ -90,21 +90,22 @@ if __name__ == "__main__":
             heatFluxList.append(heatFlux)
         heatFluxDict[datasetKey] = {"qliq": heatFluxList, "time": fileNumList}
 
-    with open("PoolBoiling-HeatFlux-0.1.json", "w") as heatFluxFile:
+    with open("PoolBoiling-HeatFlux-0.02.json", "w") as heatFluxFile:
         heatFluxFile.write(json.dumps(heatFluxDict))
 
-    pyplot.rc("font", family="serif", size=10, weight="bold")
-    pyplot.rc("axes", labelweight="bold", titleweight="bold")
-    pyplot.rc("text", usetex=True)
-    pyplot.figure(figsize=(3, 2), dpi=200)
+    #pyplot.rc("font", family="serif", size=10, weight="bold")
+    #pyplot.rc("axes", labelweight="bold", titleweight="bold")
+    #pyplot.rc("text", usetex=True)
+    #pyplot.figure(figsize=(3, 2), dpi=200)
     # pyplot.plot(
     #    heatFluxDict["Reference"]["time"],
     #    signal.savgol_filter(heatFluxDict["Reference"]["qliq"], 4, 3),
     # )
-    pyplot.plot(
-        heatFluxDict["Buffer = 4.0"]["time"],
-        signal.savgol_filter(heatFluxDict["Buffer = 4.0"]["qliq"], 20, 5),
-    )
-    pyplot.ylabel(r"$q_L$")
-    pyplot.xlabel(r"$t$")
-    pyplot.savefig("poolboiling.png")
+    #pyplot.plot(
+    #    heatFluxDict["Buffer = 4.0"]["time"],
+    #    heatFluxDict["Buffer = 4.0"]["qliq"],
+    #    #signal.savgol_filter(heatFluxDict["Buffer = 4.0"]["qliq"], 20, 5),
+    #)
+    #pyplot.ylabel(r"$q_L$")
+    #pyplot.xlabel(r"$t$")
+    #pyplot.savefig("poolboiling.png")
--- sites/summit/modules.sh
+++ sites/summit/modules.sh
@@ -5,3 +5,6 @@ module load python/3.8-anaconda3
 # Load HDF5 module in desired configuration if available. If not specified
 # the HDF5 will be built when setting up software
 module load hdf5/1.10.7
+
+# load git-lfs
+module load git-lfs
