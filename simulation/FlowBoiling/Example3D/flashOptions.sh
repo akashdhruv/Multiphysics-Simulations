b113652a7f952821b18ba37187102ce1bfbecf98
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-07-09 00:10:08 -0500

    example 3D

---
+++ simulation/FlowBoiling/Example3D/flashOptions.sh
@@ -0,0 +1,4 @@
+# cache the value of current working directory
+
+FlashOptions="incompFlow/FlowBoiling -auto -maxblocks=100 +amrex +parallelIO \
+   	      -site=$SiteHome SimForceInOut=True IOWriteGridFiles=True -3d -nxb=16 -nyb=16 -nzb=16"
