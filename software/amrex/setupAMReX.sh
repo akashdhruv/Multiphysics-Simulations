8ddea06872cda001be1ec36c9a4b0b91ccc0f2f5
Akash Dhruv <akashdhruv@gwmail.gwu.edu>
2023-10-04 10:22:37 -0500

    update setup scripts

--- software/amrex/setupAMReX.sh
+++ software/amrex/setupAMReX.sh
@@ -12,13 +12,13 @@ else
 fi
 
 # configure and install amrex in 2D
-make clean
+make clean || true
 ./configure --dim=2 --prefix=$AMREX2D_HOME
 make -j
 make install
 
 # configure and install amrex in 3D
-make clean
+make clean || true
 ./configure --dim=3 --prefix=$AMREX3D_HOME
 make -j
 make install
